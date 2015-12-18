2e970c0fe16a95f50a93135a97ec25b3948c9d8e
OksanaL <oklalak@gmail.com>
2015-12-18 17:56:53 +0200

    Update task3.php

--- task3.php
+++ task3.php
@@ -4,18 +4,19 @@ $A = array('a', 'b', 'c', 'd');
 $l = count($A);
 echo "<pre>";
 for ($i = 0; $i < $l; $i++) {
-    getPlacement($i, $A, $l);
+    getPlacement($i, $A);
 }
 //var_dump($plt);
 
-function getPlacement($i, $A, $l, $n = 0, $B = array()){
+function getPlacement($i, $A, $B = array()){
     $B[] = $A[$i];
     print_r($B);
-    $n++;
+    $l = count($A);
+    $n = count($B);
     if($n < $l){
         for($j = 0; $j < $l; $j++){
             if(!in_array($A[$j], $B)){
-                getPlacement($j, $A, $l, $n, $B);
+                getPlacement($j, $A, $B);
             }
         }
     }
